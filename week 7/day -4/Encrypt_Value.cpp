a69e772d3a76d8e9e7e5602852c159523e147d8d
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-05-06 21:18:31 +0600

    Add files via upload

---
+++ week 7/day -4/Encrypt_Value.cpp
@@ -0,0 +1,70 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-05-06 20:57:50
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define INF LLONG_MAX
+#define MOD 1000000007
+#define pb push_back
+#define vvi vector<vector<int>>
+#define vpi vector<pair<int, int>>
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+void solve()
+{
+    int n;
+    cin >> n;
+    long long s = 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s <= 1 || a[i] == 1)
+        {
+            s += a[i];
+        }
+        else
+        {
+            s = (s * a[i]) % MOD;
+        }
+    }
+
+    cout << s << '\n';
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    // int t = 1;
+    while (tc--)
+    {
+        // cout << "Case #" << t++ << ": ";
+        solve();
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
