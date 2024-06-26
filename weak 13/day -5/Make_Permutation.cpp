dc2b64be03f7278bc875ef6b7a2c312751e9dd6b
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-06-26 22:16:47 +0600

    Add files via upload

---
+++ weak 13/day -5/Make_Permutation.cpp
@@ -0,0 +1,103 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-06-26 20:43:09
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// template <typename T>
+// using set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl cout << "\n"
+#define PI 3.1415926535897932384626433832795
+#define vi vector<int>
+#define read(a)       \
+    for (auto &i : a) \
+    cin >> i
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+void solve()
+{
+    /* Lets GO */
+    int s;
+    cin >> s;
+    vi v(s);
+    read(v);
+    sort(v.begin(), v.end());
+    vector<bool> used(s + 1, false);
+    for (int i = 0; i < s; ++i)
+    {
+        bool found = false;
+        for (int x = 1; x <= s; ++x)
+        {
+            if (!used[x] && v[i] <= x)
+            {
+                used[x] = true;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+    return;
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
---
+++ weak 13/day -5/Yoga_Class.cpp
@@ -0,0 +1,90 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-06-26 20:33:54
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl cout << "\n"
+#define PI 3.1415926535897932384626433832795
+#define vi vector<int>
+#define read(a)       \
+    for (auto &i : a) \
+    cin >> i
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+void solve()
+{
+    /* Lets GO */
+    int N, X, Y;
+    cin >> N >> X >> Y;
+
+    vector<int> D(N + 1);
+    D[0] = 0;
+    D[1] = X;
+
+    for (int i = 2; i <= N; ++i)
+    {
+        D[i] = max(D[i - 1] + X, D[i - 2] + Y);
+    }
+
+    cout << D[N];
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
