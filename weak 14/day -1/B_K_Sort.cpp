c526a23659710a90d41a96d3679f4efddd71d5b2
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-06-30 23:28:04 +0600

    Add files via upload

---
+++ weak 14/day -1/B_K_Sort.cpp
@@ -0,0 +1,110 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-06-30 20:39:37
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl cout << "\n"
+#define PI 3.1415926535897932384626433832795
+#define vi vector<ll>
+#define read(a)       \
+    for (auto &i : a) \
+    cin >> i
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+void solve()
+{
+    /* Lets GO */
+    ll s;
+    cin >> s;
+    vi v(s);
+    read(v);
+    ll ans = 0, y = 0;
+
+    priority_queue<ll> pq;
+    ll x = v[0];
+    for (int i = 0; i < s; i++)
+    {
+        x = max(x, v[i]);
+        if (x > v[i])
+        {
+            pq.push(x - v[i]);
+        }
+    }
+
+    if (pq.empty())
+    {
+        cout << 0;
+        return;
+    }
+
+    ans = pq.top();
+
+    while (!pq.empty())
+    {
+        ans += (pq.top() - y) * pq.size();
+        y = pq.top();
+        pq.pop();
+    }
+
+    cout << ans;
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
