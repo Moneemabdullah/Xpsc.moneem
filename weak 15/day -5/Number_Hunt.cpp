6e0b26fb84c5b63ac68b3d77d1eb45967f570f50
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-07-10 23:27:54 +0600

    Add files via upload

---
+++ weak 15/day -5/Number_Hunt.cpp
@@ -0,0 +1,126 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-07-10 21:45:33
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 1e9 + 5;
+
+#define ll long long
+#define pb push_back
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl cout << "\n"
+#define PI 3.1415926535897932384626433832795
+#define vi vector<int>
+#define read(a)       \
+    for (auto &i : a) \
+    cin >> i
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+vector<bool> prime(N, true);
+void seave()
+{
+    prime[0] = prime[1] = false;
+    for (int i = 0; i * i < N; i++)
+    {
+        if (prime[i])
+        {
+            for (int j = i * i; j < N; j += i)
+            {
+                prime[j] = false;
+            }
+        }
+    }
+}
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+
+void solve()
+{
+    /* Lets GO */
+    ll s;
+    cin >> s;
+
+    ll p1 = s;
+    while (!isPrime(p1))
+    {
+        p1++;
+    }
+
+    ll p2 = p1 + 1;
+
+    while (!isPrime(p2))
+    {
+        p2++;
+    }
+
+    cout << static_cast<long long>(p1) * p2;
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+    // seave();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
