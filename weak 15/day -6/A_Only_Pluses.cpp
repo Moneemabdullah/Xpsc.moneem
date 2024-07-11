2692c21eb36ad509285f9020e8374b056fd1a9e3
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-07-11 23:28:18 +0600

    Add files via upload

---
+++ weak 15/day -6/A_Only_Pluses.cpp
@@ -0,0 +1,89 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-07-11 20:36:13
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl cout << "\n"
+#define PI 3.1415926535897932384626433832795
+#define vi vector<int>
+#define read(a)       \
+    for (auto &i : a) \
+    cin >> i
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+void solve()
+{
+    /* Lets GO */
+    vi v(3);
+    read(v);
+    sort(v.begin(), v.end());
+    for (int i = 0; i < 5; i++)
+    {
+        v[0]++;
+        sort(v.begin(), v.end());
+    }
+
+    cout << v[0] * v[1] * v[2];
+
+    // print_v(v);
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
