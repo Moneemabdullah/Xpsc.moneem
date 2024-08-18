d6a97163d20c563420202d2615c4f9270c6d0230
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-08-18 23:37:38 +0600

    Add files via upload

---
+++ weak 17/day 8/A_Segment_Tree_for_the_Sum.cpp
@@ -0,0 +1,172 @@
+/*
+
+         ####  ###  #   # #####        ##### ####  ##### #####
+        #     #   # #   # #            #     #   # #     #
+         ###  ##### #   # ####   AND   ####  ####  ####  ####
+            # #   #  # #  #            #     #  #  #     #
+        ####  #   #   #   #####        #     #   # ##### #####
+
+
+        #####   ###   #      #####   ####  #####  ###  #   #  #####
+        #   #  #   #  #      #      #        #     #   ##  #  #
+        #####  #####  #      ####    ###     #     #   # # #  ####
+        #      #   #  #      #          #    #     #   #  ##  #
+        #      #   #  #####  #####  ####     #    ###  #   #  #####
+
+
+*/
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-08-18 20:18:54
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAX_N = 1e5 + 9, inf = 1e9 + 9;
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define vi vector<int>
+#define vll vector<long long>
+#define pura(a) (a).begin(), (a).end()
+#define u_pura(a) (a).rbegin(), (a).rend()
+
+#define vin(a)         \
+    for (auto &it : a) \
+        cin >> it;
+#define vout(a)            \
+    for (auto &it : a)     \
+        cout << it << " "; \
+    cout << "\n";
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll factorial(ll n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+const int maxN = 1e5 + 9;
+long long a[maxN], t[4 * maxN];
+
+void build(int n, int b, int e)
+{
+    if (b == e)
+    {
+        t[n] = a[b];
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    t[n] = t[l] + t[r];
+}
+
+void update(int n, int b, int e, int i, int v)
+{
+    if (i < b || i > e)
+    {
+        return;
+    }
+    if (b == e)
+    {
+        t[n] = v;
+        return;
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    update(l, b, mid, i, v);
+    update(r, mid + 1, e, i, v);
+    t[n] = t[l] + t[r];
+}
+
+long long query(int n, int b, int e, int i, int j)
+{
+    if (e < i || j < b)
+    {
+        return 0;
+    }
+    if (b >= i && e <= j)
+    {
+        return t[n];
+    }
+    int mid = (b + e) / 2, l = 2 * n, r = (2 * n) + 1;
+    return query(l, b, mid, i, j) + query(r, mid + 1, e, i, j);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+
+    build(1, 1, n);
+    while (m--)
+    {
+        int t;
+        cin >> t;
+        if (t == 1)
+        {
+            int idx, v;
+            cin >> idx >> v;
+            idx++;
+            update(1, 1, n, idx, v);
+        }
+        else
+        {
+            int l, r;
+            cin >> l >> r;
+            l++;
+            long long ans = query(1, 1, n, l, r);
+            cout << ans << '\n';
+        }
+    }
+    return 0;
+}
