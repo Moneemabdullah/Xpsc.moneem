9c447c0b6a4b580be811f46cc537aeec2fd87f8a
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-09-06 22:56:43 +0600

    Add files via upload

---
+++ weak 19/day 2/F_Sakurako_s_Box.cpp
@@ -0,0 +1,173 @@
+/*
+
+         ####  ###  #   # #####        ##### ####  ##### #####
+        #     #   # #   # #            #     #   # #     #
+         ###  ##### #   # ####   AND   ####  ####  ####  ####
+            # #   #  # #  #            #     #  #  #     #
+        ####  #   #   #   #####        #     #   # ##### #####
+
+        #####   ###   #      #####   ####  #####  ###  #   #  #####
+        #   #  #   #  #      #      #        #     #   ##  #  #
+        #####  #####  #      ####    ###     #     #   # # #  ####
+        #      #   #  #      #          #    #     #   #  ##  #
+        #      #   #  #####  #####  ####     #    ###  #   #  #####
+
+*/
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-09-01 21:59:04
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define vi vector<int>
+#define vll vector<long long>
+#define pura(a) (a).begin(), (a).end()
+#define u_pura(a) (a).rbegin(), (a).rend()
+
+#define vin(a)         \
+    for (auto &it : a) \
+        cin >> it;
+#define vout(a)            \
+    for (auto &it : a)     \
+        cout << it << " "; \
+    cout << "\n";
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll factorial(ll n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+ll fn(ll a, ll m)
+{
+    ll m0 = m, t, q;
+    ll x0 = 0, x1 = 1;
+    if (m == 1)
+        return 0;
+    while (a > 1)
+    {
+        q = a / m;
+        t = m;
+        m = a % m;
+        a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+    if (x1 < 0)
+        x1 += m0;
+    return x1;
+}
+
+ll Exp(ll x, ll y, ll mod)
+{
+    ll result = 1;
+    x = x % mod;
+    while (y > 0)
+    {
+        if (y % 2 == 1)
+        {
+            result = (result * x) % mod;
+        }
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return result;
+}
+ll cal(ll x)
+{
+    return (1LL * x * (x - 1) % MOD);
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vll a(n);
+    ll sall = 0, squ = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+        sall = (sall + a[i]) % MOD;
+        squ = (squ + (a[i] * a[i]) % MOD) % MOD;
+    }
+
+    ll S = (sall * sall - squ + MOD) % MOD;
+
+    if (S < 0)
+        S += MOD;
+
+    S = (S * Exp(2, MOD - 2, MOD)) % MOD;
+
+    ll Q = (cal(n) * Exp(2, MOD - 2, MOD)) % MOD;
+
+    ll result = (S * Exp(Q, MOD - 2, MOD)) % MOD;
+
+    cout << result;
+}
+
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+        cout << "\n";
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
