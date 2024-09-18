17bae7f7ff86fc6184b4d6ff5c84ba2028616d48
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-09-18 14:33:13 +0600

    Add files via upload

---
+++ weak 20/day 7/D_Alter_the_GCD.cpp
@@ -0,0 +1,178 @@
+/*
+
+         ####  ###  #   # #####        ##### ####  ##### #####
+        #     #   # #   # #            #     #   # #     #
+         ###  ##### #   # ####   AND   ####  ####  ####  ####
+            # #   #  # #  #            #     #  #  #     #
+        ####  #   #   #   #####        #     #   # ##### #####
+
+
+        #####   ###   #      #####   ####  #####  ###  #   #  #####
+        #   #  #   #  #      #      #        #     #   ##  #  #
+        #####  #####  #      ####    ###     #     #   # # #  ####
+        #      #   #  #      #          #    #     #   #  ##  #
+        #      #   #  #####  #####  ####     #    ###  #   #  #####
+
+*/
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-09-14 22:31:27
+*/
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define BISMILLAH                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int MAX_N = 1e5 + 5;
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define no cout << "NO"
+#define yes cout << "YES"
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define vi vector<int>
+#define vll vector<long long>
+#define pura(a) (a).begin(), (a).end()
+#define u_pura(a) (a).rbegin(), (a).rend()
+
+#define vin(a)         \
+    for (auto &it : a) \
+        cin >> it;
+#define vout(a)            \
+    for (auto &it : a)     \
+        cout << it << " "; \
+    cout << "\n";
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll factorial(ll n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*  --------------------SOLUTION PROGRAM-------------------------*/
+int gcd_of_vector(
+    vector<int> &v)
+{
+    int result = v[0];
+    for (int i = 1; i < v.size(); i++)
+    {
+        result = gcd(result, v[i]);
+    }
+    return result;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    int gcd_a = gcd_of_vector(a);
+    int gcd_b = gcd_of_vector(b);
+    int initial_sum = gcd_a + gcd_b;
+
+    int max_gcd_sum = initial_sum;
+    int max_count = 0;
+
+    for (int l = 0; l < n; l++)
+    {
+        for (int r = l; r < n; r++)
+        {
+
+            vector<int> new_a = a;
+            vector<int> new_b = b;
+
+            for (int i = l; i <= r; i++)
+            {
+                swap(new_a[i], new_b[i]);
+            }
+
+            int gcda = gcd_of_vector(new_a);
+            int gcdb = gcd_of_vector(new_b);
+            int new_sum_gcd = gcda + gcdb;
+
+            if (new_sum_gcd > max_gcd_sum)
+            {
+                max_gcd_sum = new_sum_gcd;
+                max_count = 1;
+            }
+            else if (new_sum_gcd == max_gcd_sum)
+            {
+                max_count++;
+            }
+        }
+    }
+
+    cout << max_gcd_sum << " " << max_count << endl;
+}
+/*  --------------------MAIN PROGRAM----------------------------*/
+
+int main()
+{
+    BISMILLAH
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        // cout << "Case #" << tc++ << ": ";
+        solve();
+    }
+    return 0;
+}
+/* -----------------END OF PROGRAM --------------------*/
+
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
